d077fb839be3554465c1217040cce10a2126f361
izyb <isaacbeckie@gmail.com>
2018-10-20 14:23:26 -0700

    first working prototype

--- src/delaunay.js
+++ src/delaunay.js
@@ -117,8 +117,48 @@ const getEdgePoints = (imageData) => {
       }
     }
   }
-  console.log(points.length);
   return points;
+}
+
+const ccw = (p1, p2, p3) => {
+  return (p2[1] - p1[1]) * (p3[0] - p1[0]) - (p3[1] - p1[1]) * (p2[0] - p1[0]) > 0;
+}
+
+const sortCCW = (points) => {
+  let temp;
+  const newPoints = points.slice();
+  for (let i = 0; i < newPoints.length; i += 1) {
+    if (newPoints[i][1] < newPoints[0][1] || (newPoints[i][1] === newPoints[0][1] && newPoints[i][0] < newPoints[0][0])) {
+      temp = newPoints[i].slice();
+      newPoints[i] = newPoints[0].slice();
+      newPoints[0] = temp.slice();
+    }
+  }
+
+  for (let i = 1; i < newPoints.length - 1; i += 1) {
+    for (let j = 1; j < newPoints.length - i; j += 1) {
+      if (!ccw(newPoints[i + j], newPoints[0], newPoints[i])) {
+        temp = newPoints[i].slice();
+        newPoints[i] = newPoints[i + j].slice();
+        newPoints[i + j] = temp.slice();
+      }
+    }
+  }
+  return newPoints;
+}
+
+const det = (A) => {
+  return A[0] * (A[4] * A[8] - A[7] * A[5]) - A[1] * (A[3] * A[8] - A[6] * A[5]) + A[2] * (A[3] * A[7] - A[6] * A[4]);
+}
+
+const getAngle = (s1, s2) => {
+  const v1 = [s1[1][0] - s1[0][0], s1[1][1] - s1[0][1]];
+  const v2 = [s2[1][0] - s2[0][0], s2[1][1] - s2[0][1]];
+  const m1 = Math.sqrt(v1[0] * v1[0] + v1[1] * v1[1]);
+  const m2 = Math.sqrt(v2[0] * v2[0] + v2[1] * v2[1]);
+  console.log(m1, m2);
+  console.log(s1, s2);
+  return Math.acos((v1[0] * v2[0] + v1[1] * v2[1]) / (m1 * m2));
 }
 
 class Delaunay {
@@ -155,20 +195,146 @@ class Delaunay {
       trianglePoints[i] = points[j];
       points.splice(j, 1);
     }
-    console.log(trianglePoints);
-    const datas = [imageData, grayScaleData, blurredData, edgeData];
+    const triangles = this.build(trianglePoints, imageData);
     let i = 0;
+    targetCtx.putImageData(edgeData, 0, 0);
+
+    targetCtx.fillStyle = 'blue';
+    points.forEach(p => targetCtx.fillRect(p[0], p[1], 1, 1));
+
     setInterval(() => {
-      targetCtx.putImageData(datas[i], 0, 0);
-      i = i < datas.length - 1 ? i + 1 : 0;
-      targetCtx.fillStyle = 'blue';
-      points.forEach(p => targetCtx.fillRect(p[0], p[1], 1, 1));
-      targetCtx.fillStyle = 'red';
-      trianglePoints.forEach(p => targetCtx.fillRect(p[0], p[1], 1, 1));
-    }, 700);
-    // Grayscale + blur, edge detection convolution filters.
-    // Grab all pronounced edge points (exposed from edge detection).
-    // Take random sample from these points, put through delaunay algorithm
-    // Average colors?
+      targetCtx.beginPath();
+      targetCtx.strokeStyle = 'red';
+      const {
+        a,
+        b,
+        c
+      } = triangles[i];
+      targetCtx.moveTo(a[0], a[1]);
+      targetCtx.lineTo(b[0], b[1]);
+      targetCtx.lineTo(c[0], c[1]);
+      targetCtx.lineTo(a[0], a[1]);
+
+      const colorI = 4 * (Math.round((a[0] + b[0] + c[0]) / 3) + Math.round((a[1] + b[1] + c[1]) / 3) * width);
+
+      targetCtx.fillStyle = `rgb(${imageData.data[colorI]}, ${imageData.data[colorI + 1]}, ${imageData.data[colorI + 2]})`;
+      targetCtx.fill();
+      i = i < triangles.length - 1 ? i + 1 : 0;
+    }, 5);
+  }
+
+  build(points, imageData) {
+    const {
+      width,
+      height
+    } = imageData;
+    let triangles = [
+      new Triangle([
+        [0, 0],
+        [width, 0],
+        [width, height]
+      ]),
+      new Triangle([
+        [0, 0],
+        [width, height],
+        [0, height]
+      ]),
+    ];
+    let tempEdges = [];
+    for (let i = 0; i < points.length; i += 1) {
+      for (let j = 0; j < triangles.length; j += 1) {
+        if (triangles[j].contains(points[i])) {
+          tempEdges.push(...triangles[j].getEdges());
+          triangles.splice(j, 1);
+          j--;
+        }
+      }
+      const newTriangles = Triangle.flip(tempEdges, points[i]);
+      triangles.push(...newTriangles);
+      tempEdges = [];
+    }
+
+    return triangles;
+  }
+}
+
+const pointEq = (a, b) => a[0] === b[0] && a[1] === b[1];
+
+const edgeEq = (a, b) => {
+  return (
+    (pointEq(a[0], b[0]) && pointEq(a[1], b[1])) ||
+    (pointEq(a[1], b[0]) && pointEq(a[0], b[1]))
+  );
+}
+
+class Triangle {
+  static flip(tEdges, d) {
+    const hull = [];
+    const newTriangles = [];
+    tEdges.forEach(edge => {
+      const i = hull.findIndex(e => edgeEq(e, edge));
+      if (i !== -1) {
+        hull.splice(i, 1);
+      } else {
+        hull.push(edge);
+      }
+    });
+    for (let i = 0; i < hull.length; i += 1) {
+      newTriangles.push(new Triangle([hull[i][0], hull[i][1], d]));
+    }
+    return newTriangles;
+  }
+
+  constructor(vertices) {
+    if (vertices.length !== 3) {
+      throw new Error('3 vertices PLEASE.');
+    }
+    vertices = sortCCW(vertices);
+    const [a, b, c] = vertices;
+
+    const [abx, aby] = [b[0] - a[0], b[1] - a[1]];
+    const [acx, acy] = [c[0] - a[0], c[1] - a[1]];
+    const t = b[0] * b[0] + b[1] * b[1] - a[0] * a[0] - a[1] * a[1];
+    const u = c[0] * c[0] + c[1] * c[1] - a[0] * a[0] - a[1] * a[1];
+    const s = Math.pow(2 * (abx * acy - aby * acx), -1);
+    const circle = [
+      ((acy) * t - (aby) * u) * s,
+      ((abx) * u - (acx) * t) * s
+    ];
+    const [dx, dy] = [a[0] - circle[0], a[1] - circle[1]];
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.circle = circle;
+    this.rSq = dx * dx + dy * dy;
+  }
+
+  getEdges() {
+    return [
+      [this.a, this.b],
+      [this.b, this.c],
+      [this.c, this.a],
+    ];
+  }
+
+  contains(d) {
+    const {
+      circle,
+      rSq,
+    } = this;
+    const [dx, dy] = [d[0] - circle[0], d[1] - circle[1]];
+    return dx * dx + dy * dy < rSq;
+  }
+
+  containsPoint(d) {
+    const { a, b, c } = this;
+
+    const area = 1 / 2 * (-b[1] * c[0] + a[1] * (-b[0] + c[0]) + a[0] * (b[1] - c[1]) + b[0] * c[1]);
+    const sign = area < 0 ? -1 : 1;
+    const s = (a[1] * c[0] - a[0] * c[1] + (c[1] - a[1]) * d[0] + (a[0] - c[0]) * d[1]) * sign;
+    const t = (a[0] * b[1] - a[1] * b[0] + (a[1] - b[1]) * d[0] + (b[0] - a[0]) * d[1]) * sign;
+
+    return s > 0 && t > 0 && (s + t) < 2 * area * sign;
   }
 }
